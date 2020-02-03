f009ec712d601a9b83614c9cb4bdc5faca9749b0
Trevor Leffert <leffertt@gmail.com>
2020-02-02 22:03:26 -0600

    Added win checking for rows and columns

--- src/board/Board.tsx
+++ src/board/Board.tsx
@@ -9,6 +9,7 @@ export interface BoardState {
     turnNumber: number;
     // Current active player taking thier turn
     activePlayer: string;
+    winner: string;
 
 }
 
@@ -25,23 +26,82 @@ export class Board extends Component<BoardProps, BoardState> {
         this.state = {
             squares: new Array<string>(props.edgeLength * props.edgeLength).fill(null),
             turnNumber: 0,
-            activePlayer: 'X'
+            activePlayer: 'X',
+            winner: null
         }
     }
 
     // Assigns the select square to the current active player
     // and advances the turn state with updated active player
-    assignSquare(pos: number) {
+    private assignSquare(pos: number) {
         const squares = this.state.squares.slice();
         squares[pos] = this.state.activePlayer;
+        let currentActive = this.state.activePlayer;
 
+        // Set new state then checkfor a player win
         this.setState({
             squares: squares,
             turnNumber: this.state.turnNumber+1,
             activePlayer: this.state.turnNumber % 2 !== 0 ? 'X' : 'O'
+        }, () => {
+            let hasWinner = this.checkForPlayerWin(currentActive);
+            if(hasWinner) {
+                this.setState({
+                    winner: currentActive
+                })
+            }
         });
     }
 
+    private checkForPlayerWin(player: string): boolean {
+        // TODO Check  both diagonal
+        for(let i = 0; i < this.props.edgeLength; i++) {
+            if(this.checkColWin(i, player)) {
+                return true;
+            }
+
+            if(this.checkRowWin(i, player)) {
+                return true;
+            }
+
+        }
+    }
+
+    /**
+     * Checks the provided row for a win
+     * @type {boolean}
+     */
+    private checkRowWin(row: number, player: string): boolean {
+        let score = 0;
+        for(let i = 0; i < this.props.edgeLength; i++) {
+            score += this.checkSquareMatches(i + (row * this.props.edgeLength), player);
+        }
+        // TODO make score to win dynamic
+        return score === 3;
+    }
+
+    /**
+     * Checks the provided col for a win
+     * @type {boolean}
+     */
+    private checkColWin(col: number, player: string): boolean {
+        let score = 0;
+        for(let i = 0; i < this.props.edgeLength; i++) {
+            score += this.checkSquareMatches(col + (i * this.props.edgeLength), player);
+        }
+        // TODO make score to win dynamic
+        return score === 3;
+    }
+
+    private checkSquareMatches(pos: number, owner: string) {
+        return owner === this.state.squares[pos] ? 1 : 0;
+    }
+
+    /**
+     * Returns <Square> element to be rendered with associated id
+     * @param  {number} id id to associate with the square
+     * @return {[type]}    Square element to be rendered
+     */
     renderSquare(id: number) {
         return (
             <Square
@@ -61,6 +121,8 @@ export class Board extends Component<BoardProps, BoardState> {
         let row = [];
         for(let i = 0; i < numSquares; i++) {
             row.push(
+                // posMultiplier * edgeLength will give you what row number to start at
+                // + i will give you the column of the square (when thinking in 2d array)
                 this.renderSquare(i + (posMultiplier * this.props.edgeLength))
             );
         }
@@ -80,6 +142,9 @@ export class Board extends Component<BoardProps, BoardState> {
 
     return (
         <div>
+            {
+                this.state.winner ? (<div className="winner">{this.state.winner} IS WIN!</div>) : null
+            }
             <div className="status">{this.state.activePlayer}'s turn!</div>
             <div className="board">
                 {boardRows}
